3394b23c5cc96e64e1b0f94158eacac6447bb9a9
Nooooooo <113202604+Nooooooo-0328@users.noreply.github.com>
2024-10-20 16:16:24 +0900

    Add files via upload

--- temp/script.js
+++ temp/script.js
@@ -1,5 +1,6 @@
 async function fetchEarthquakeData() {
     const response = await fetch("https://api.p2pquake.net/v2/history?codes=551&limit=1");
+    // const response = await fetch("https://api.p2pquake.net/v2/jma/quake?limit=1&order=-1&quake_type=DetailScale&min_scale=70"); # テストデータ 震度7
     const js_l = await response.json();
 
     let hypocenter = js_l[0]["earthquake"]["hypocenter"]["name"];
@@ -105,7 +106,6 @@ async function fetchEarthquakeData() {
     }
 
     let singen = hypocenter || '-';
-    let singen_j = hypocenter ? `#${hypocenter} ` : '';
     let magu = magnitude !== -1 ? `M${magnitude}` : '-';
     let hukasa = depth !== -1 ? `約${depth}km` : '-';
 
@@ -142,30 +142,33 @@ async function fetchEarthquakeData() {
         pointsText += point;
     }
 
-    version = "v 2.2.0"
-    guidance = "   | [📌 ご案内] NanbuCameraの放送をご視聴いただきましてありがとうございます。本放送をご覧になられる方は必ず概要欄をご確認ください。 [🛑 本放送について] 本放送では、日本/韓国/台湾で地震が発生した際、音声と映像でお知らせいたします。急に音声が流れますので、音量にご注意ください。 [💻 使用ソフト] 本放送で使用しているソフトは以下の通りです。*印が付いているソフトは非配布ソフトです。 YDITS for Web / Nanbu Eq Service Bot Dev* / 強震モニタ / 사용자 맞춤형 지진정보서비스 / 地牛Wake Up! / Nanbu テロップ / Nanbu 時計 [🙏 開発者の皆様に感謝いたします。 | ⚠️ 配信の安定性について] 本放送は、できる限り安定した放送を心がけていますが、予期せぬトラブルにより配信が一時停止する場合がございます。その際は、ご理解のほどよろしくお願いいたします。 [📬 お問い合わせ] 本放送に関する、お問い合わせ又はご質問はX(旧: Twitter) @NanbuCamera まで、お願いいたします。 | " + version
+    version = " | 本テロップのバージョンは Ver 2.3.0 です。最終更新日: 10月20日"
+    text_1 = "[📌放送案内] NanbuCameraの地震監視放送をご視聴いただきましてありがとうございます。"
+    text_2 = "[🛑本放送について] 本放送では、日本/韓国/台湾 で地震が発生した際、音声と映像でお知らせいたします。急に音声が流れますので、音量にご注意ください。" 
+    text_3 = "[📬お問い合わせ] 本放送に関する、お問い合わせ又はご質問は X(旧: Twitter) @NanbuCamera まで、お願いいたします。"
+    guidance = text_1 + text_2 + text_3 + version
 
     let info;
     switch (type) {
         case "ScalePrompt":
             info = "震度速報";
-            text = `[📣 ${info}] ${jmaDatetime_time}ごろ、最大震度${shindo_}を観測する地震がありました。新しい情報が入り次第お伝え致します。`
+            text = `[📣${info}] ${jmaDatetime_time}ごろ、最大震度${shindo_}を観測する地震がありました。新しい情報が入り次第お伝え致します。`
             break;
         case "Destination":
             info = "震源に関する情報";
-            text = `[📣 ${info}] ${jmaDatetime_time}ごろ、${singen}で地震がありました。を観測する地震がありました。新しい情報が入り次第お伝え致します。`
+            text = `[📣${info}] ${jmaDatetime_time}ごろ、${singen}で地震がありました。新しい情報が入り次第お伝え致します。`
             break;
         case "ScaleAndDestination":
             info = "震源 ・ 震度に関する情報";
-            text = `[📣 ${info}] ${jmaDatetime_time}ごろ、${singen}を震源とする、最大震度${shindo_}を観測する地震がありました。`
+            text = `[📣${info}] ${jmaDatetime_time}ごろ、${singen}を震源とする、最大震度${shindo_}を観測する地震がありました。新しい情報が入り次第お伝え致します。`
             break;
         case "DetailScale":
             info = "各地の震度に関する情報";
-            text = `[📣 ${info}] ${jmaDatetime_time}ごろ、${singen}を震源とする、最大震度${shindo_}を観測する地震がありました。マグニチュードは${magu}、深さは${hukasa}と推定されています。${domesticTsunami}` + guidance
+            text = `[📣${info}] ${jmaDatetime_time}ごろ、${singen}を震源とする、最大震度${shindo_}を観測する地震がありました。マグニチュードは${magu}、深さは${hukasa}と推定されています。${domesticTsunami}` + guidance
             break;
         case "Foreign":
             info = "遠地地震に関する情報";
-            text = `[📣 ${info}] ${jmaDatetime_time}ごろ、${singen}で地震がありました。マグニチュードは${magu}、${domesticTsunami}` + guidance
+            text = `[📣${info}] ${jmaDatetime_time}ごろ、${singen}で地震がありました。マグニチュードは${magu}、${domesticTsunami}` + guidance
             break;
         default:
             info = "その他";
@@ -177,7 +180,7 @@ async function fetchEarthquakeData() {
     const tickerElement = document.getElementById('ticker-text');
     tickerElement.innerText = tickerText;
 
-    const animationDuration = Math.max(tickerText.length * 0.1, 20);
+    const animationDuration = Math.max(tickerText.length * 0.15, 10);
     tickerElement.style.animationDuration = `${animationDuration}s`;
 }
 
